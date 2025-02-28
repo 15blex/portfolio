58db3ca5029832033dcbb581e31484c3364f4871
alexi <15blex@gmail.com>
2025-02-28 16:38:58 +0100

    Updates

--- _app/immutable/entry/start.CIbd_DTf.js
+++
@@ -1,1 +0,0 @@
-import{l as o,c as r}from"../chunks/DTOR-4_k.js";export{o as load_css,r as start};
---
+++ _app/immutable/entry/start.DdEB0GKv.js
@@ -0,0 +1,1 @@
+import{l as o,c as r}from"../chunks/CCpfbT4c.js";export{o as load_css,r as start};
