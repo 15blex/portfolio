a907f384f73f5abb535d00c9882bcee82d638dfa
alexi <15blex@gmail.com>
2025-02-28 13:09:01 +0100

    first commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import '$lib/app.css';
+	import { i18n } from '$lib/i18n';
+	import { ParaglideJS } from '@inlang/paraglide-sveltekit';
+	let { children } = $props();
+</script>
+
+<ParaglideJS {i18n}>
+	<header>H</header>
+	<main>
+		{@render children()}
+	</main>
+	<footer>F</footer>
+</ParaglideJS>
